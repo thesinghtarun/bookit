7921b4eb72e986eab24964651684cb95a1f8eb5c
thesinghtarun <thesinghtarunoficial.gmail.com>
2025-10-31 18:13:40 +0530

    done

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const controller = require('./controller')
+const controller=require('../controller/controller')
 const router=express.Router()
 
 router.route("/").get((req,res)=>{res.send("HomePage")})
